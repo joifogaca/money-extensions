8a752b511ef1c375015ccf818a86ed94e13cbbeb
Joice Fogaça <joi.fogaca@gmail.com>
2024-03-10 12:48:20 -0300

    Finalizado troca de nomes

--- MoneyExtension/MoneyExtension.cs
+++ MoneyExtension/MoneyExtension.cs
@@ -2,16 +2,17 @@
 
 public static class Money
 {
-    public static int ToCents(this decimal amount) { 
+    public static int ToCents(this decimal amount)
+    {
         if (amount <= 0) return 0;
 
-        var text = amount.ToString("N2")
-            .Replace(",","")
-            .Replace(".","");
+        var value = amount.ToString("N2")
+            .Replace(",", "")
+            .Replace(".", "");
 
-        if(string.IsNullOrEmpty(text)) return 0;
+        if (string.IsNullOrEmpty(value)) return 0;
 
-        int.TryParse(text, out var cents);
+        int.TryParse(value, out var cents);
         return cents;
     }
 
